c30cbb5cc71b3aa4e3ba45437da9b98ed1b0e8a4
ElizavetaShikhova <elizaveta.liza06@mail.ru>
2024-05-15 19:26:38 +0500

    game over was added

--- Game/Assets/Scripts/GameState.cs
+++ Game/Assets/Scripts/GameState.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameState : MonoBehaviour
 {
     public static bool gameIsPaused;
     [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject gameOver;
+    private bool isGameOver = false;
 
-    void Start() => pauseMenu.SetActive(false);
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+        gameOver.SetActive(false);
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) LoadMenu();
+        if (Input.GetKeyDown(KeyCode.Escape) && !isGameOver) LoadMenu();
     }
 
     public void Resume()
@@ -40,5 +47,16 @@ public class GameState : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void GameOver()
+    {
+        gameOver.SetActive(true);
+        isGameOver = true;
+        var players = GameObject.FindGameObjectsWithTag("Player");
+        var (player1, player2) = (players[0].GetComponent<Player>(), players[1].GetComponent<Player>());
+        (player1.enabled, player2.enabled) = (false, false);
+        //GameObject.FindWithTag("Player1").GetComponent<Player>().enabled = false;
+        //GameObject.FindWithTag("Player2").GetComponent<Player>().enabled = false;
     }
 }
--- Game/Assets/Scripts/Player/PlayerHealth.cs
+++ Game/Assets/Scripts/Player/PlayerHealth.cs
@@ -4,29 +4,35 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private float maxHealth = 10;
+    private GameState gameState;
     public float currentHealth { get; private set; }
-    private bool dead;
+    private Animator anim;
 
     void Awake()
     {
         currentHealth = maxHealth;
+        anim = GetComponent<Animator>();
+        gameState = FindObjectOfType<GameState>();
     }
 
     public void TakeDamage(float damage)
     {
         currentHealth = Mathf.Clamp(currentHealth - damage, 0, maxHealth);
-
         if (currentHealth <= 0)
-        {
             Die();
-        }
     }
 
-    void Die()
+    public void Die()
     {
-        if (!dead)
-        {
-            GetComponent<Player>().enabled = false;
-        }
+        anim.SetBool("run", false);
+        StartCoroutine(HandleDeath());
+    }
+
+    IEnumerator HandleDeath()
+    {
+        anim.SetTrigger("dead");
+        while (anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1)
+            yield return null;
+        gameState.GameOver();
     }
 }
