dd6833f03621db96aedce0d04c815710dce29fc2
Dabbler10 <dabbler503@gmail.com>
2024-05-23 01:15:24 +0500

    add door

--- Game/Assets/Scripts/Quests/Door.cs
+++ Game/Assets/Scripts/Quests/Door.cs
@@ -7,6 +7,8 @@ public class Door : MonoBehaviour
     public PlayerMovement player2;
     public float openDistance = 0.03f; // Расстояние, на котором двери откроются
     private Quest quest;
+    private Animator anim;
+    private BoxCollider2D collider;
 
     private bool isOpen = false;
     private bool isClosed = true;
@@ -16,49 +18,36 @@ public class Door : MonoBehaviour
         quest = GetComponent<Quest>();
         var players = GameObject.FindGameObjectsWithTag("Player");
         (player1, player2) = (players[0].GetComponent<PlayerMovement>(), players[1].GetComponent<PlayerMovement>());
+        anim = GetComponent<Animator>();
+        collider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
-        // Проверяем, завершен ли все предыдущий квест
-        if (quest.Previous == null || !quest.Previous.IsActive)
-        {
-            // Проверяем, находятся ли игроки рядом с дверью
-            if (Vector3.Distance(player1.transform.position, transform.position) < openDistance ||
-                Vector3.Distance(player2.transform.position, transform.position) < openDistance)
-            {
-                OpenDoor();
-            }
-        }
-
-        // Проверяем, вошли ли оба игрока в комнату
-        if (IsBothPlayersInRoom() && !quest.IsActive && !quest.IsCompleted)
+        if (player1.transform.position.x > transform.position.x + 5 && player2.transform.position.x > transform.position.x + 5  && !quest.IsActive && !quest.IsCompleted)
         {
             CloseDoor();
             quest.StartQuest();
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (isClosed  && !quest.IsActive && quest.Previous == null || !quest.Previous.IsActive)
+            OpenDoor();
+    }
+
     private void OpenDoor()
     {
-        if (!isOpen)
-        {
-            // здесь должна быть анимация открытия двери
-            (isOpen, isClosed) = (true, false);
-        }
+        (isOpen, isClosed) = (true, false);
+        anim.SetBool("IsOpened", true);
+        collider.isTrigger = true;
     }
 
     private void CloseDoor()
     {
-        if (!isClosed)
-        {
-            // здесь должна быть анимация закрытия двери
-            (isOpen, isClosed) = (false, true);
-        }
-    }
-
-    private bool IsBothPlayersInRoom()
-    {
-        return GetComponent<OneWayCollider>().playersInRoom;
+        (isOpen, isClosed) = (false, true);
+        anim.SetBool("IsOpened", false);
+        collider.isTrigger = false;
     }
 }
