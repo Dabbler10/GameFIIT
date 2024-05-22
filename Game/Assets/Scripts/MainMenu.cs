1a69bce1b52fa8a82b559f50e82107e540f84ea9
ElizavetaShikhova <elizaveta.liza06@mail.ru>
2024-05-22 17:28:38 +0500

    story at the beggining was added

--- Game/Assets/Scripts/MainMenu.cs
+++ Game/Assets/Scripts/MainMenu.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+            PlayGame();
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
