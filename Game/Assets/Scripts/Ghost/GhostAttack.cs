946aff6414edb6a192b5ba5bdd8416463ec2e8c4
Dabbler10 <dabbler503@gmail.com>
2024-05-24 10:10:27 +0500

    fix

--- Game/Assets/Scripts/Ghost/GhostAttack.cs
+++ Game/Assets/Scripts/Ghost/GhostAttack.cs
@@ -6,19 +6,18 @@ using UnityEngine;
 public class GhostAttack : MonoBehaviour
 {
     [SerializeField] private float damage = 1;
-    private float attackRate = 0.5f;
+    private float attackRate = 1f;
     private float nextAttackTime = 0f;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (Time.time >= nextAttackTime)
-        {
+        if (nextAttackTime == 0f)
+            nextAttackTime = Time.time + 1f;
+        if (Time.time > nextAttackTime)
             if (collision.collider.CompareTag("Player"))
             {
-                Debug.Log("aboba");
                 collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
-                nextAttackTime = Time.time + attackRate;
+                nextAttackTime = Time.time + 1f;
             }
-        }
     }
 }
